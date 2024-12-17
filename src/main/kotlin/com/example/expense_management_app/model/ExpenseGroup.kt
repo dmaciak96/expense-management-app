0085628b54c4239abd70015f44c58572773ae250
Dawid <dawid.maciak@huuugegames.com>
2024-12-17 13:15:47 +0100

    restart project

--- src/main/kotlin/com/example/expense_management_app/model/ExpenseGroup.kt
+++
@@ -1,9 +0,0 @@
-package com.example.expense_management_app.model
-
-enum class ExpenseGroup {
-    HOME,
-    FOOD,
-    CAR,
-    TOOLS,
-    OTHER
-}
--- src/main/kotlin/com/example/expense_management_app/repository/ExpenseRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.expense_management_app.repository
-
-import com.example.expense_management_app.model.Expense
-import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
-
-interface ExpenseRepository : JpaRepository<Expense, UUID> {
-}
